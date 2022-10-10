bc3c5073f59fa626aeaa2b4e9fbe375c2b163898
chicchicchic <chicken481999@gmail.com>
2022-10-10 15:28:44 +0700

    Fetch Server Data (React Hooks)

--- frontend/src/Pages/HomeScreen.js
+++ frontend/src/Pages/HomeScreen.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+// import data from "../data";
+import axios from 'axios';
 
 function HomeScreen(props) {
+  
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios.get("/api/foods");
+      setFoods(data);
+    }
+    fetchData();
+  
+    return () => {
+      //
+    }
+  }, [])
+  
+
+
   return (
     <div>
       <h3 className="sub-heading">our dishes</h3>
       <h1 className="heading">popular dishes</h1>
 
       <ul className="dishes-container">
-        {data.foods.map((food) => (
+        {foods.map((food) => (
           <li key={food._id} className="box">
             <Link to={"/food/" + food._id} className="box-title">
               <img className="box-img" src={food.imgSource} alt="product img" />
